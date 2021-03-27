073d438c7ff5d59b72be7f23f85f4cf2fb53347d
icestormasteroid <67393771+icestormasteroid@users.noreply.github.com>
2021-03-27 00:07:01 -0400

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dsaasdas
+# IM415 Class 9: Svelte To-do App
+Use this for reference, but code your own!!
+This example is using [Bulma](https://bulma.io/) as the CSS/Layout framework (just for the delete icon, but could be used for more than that of course).
---
+++ src/Todos.svelte
@@ -0,0 +1,99 @@
+<script>
+    import { slide } from 'svelte/transition';
+    let todoItem = '';
+    let todoList = [];
+    //add items from input field into array
+    function addToList() {
+        if (!todoItem) return;
+        todoList = [...todoList, {
+            text: todoItem,
+            status: false
+        }];
+        todoItem = '';
+    }
+    
+    //remove items from array
+    function removeFromList(index) {
+        todoList.splice(index, 1);
+        todoList = todoList;        
+    }
+</script>
+
+<form on:submit|preventDefault={addToList}>
+    <input bind:value={todoItem}>
+        <button class="button is-primary">
+          <span class="icon is-small">
+            <i class="fas fa-plus"></i>
+          </span>
+        </button>
+</form>
+
+<div class="container">
+<ol class="todoList">
+    {#each todoList as item, index}
+    <li transition:slide="{{ y: 20, duration: 120 }}">
+    
+    <div class="asdjklj">
+    
+    <div class="check"><input bind:checked={item.status} type="checkbox" class="checkbox"></div>
+
+    <div class="items"><span class:checked={item.status}>{item.text}</span></div>
+    <div class="x"><button style class="delete" type="button" on:click={() => removeFromList(index)} ></button></div>
+
+    </div>
+    </li>
+    {/each}
+</ol>
+</div>
+
+<style>
+
+    .todoList {
+    list-style: none;
+    }
+
+    .checkbox {
+        background-color: purple;
+    }
+
+    .checked {
+        text-decoration: line-through;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+
+    }
+
+    .asdjklj {
+    display:flex;
+    flex-wrap:wrap;
+    flex-direction:row;
+    justify-content:space-between;
+    align-items:stretch;
+    background-color: rgb(228, 228, 228);
+    border-radius: 5px ;
+    margin: 0.8em 0em;
+    padding: 0.8em 1em;
+    }
+
+    .check {
+    order: 1;
+    flex-basis: auto;
+    }
+    
+    .items {
+    order: 2;
+    text-align: left;
+    padding: 0em 5em;
+    flex-basis: auto;
+
+    }
+
+    .x {
+    order: 3;
+    justify-content: right;    
+    flex-basis: auto;
+  }
+</style>
